bbf5c41d527306fe7e045f4bd06ec593fcc59e53
Dyson Parkes <dyson@berserkir.net>
2019-02-27 17:01:12 +1300

    Add comments

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HelloWorld
@@ -19,11 +13,14 @@ namespace HelloWorld
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            // This one calls a function, defined at the end of this file.
+            CloseApp();
         }
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            // This is link to the "About" button under the help menu.
+            // It was attached by double-clicking the item after creating it.
             AboutBox box = new AboutBox();
             box.ShowDialog();
         }
@@ -42,21 +39,34 @@ namespace HelloWorld
 
         private void buttonBold_Click(object sender, EventArgs e)
         {
-            if (labelMessage.Font.Bold)
+            if (labelMessage.Font.Bold) // Is it bold?
             {
-                labelMessage.Font = new Font(labelMessage.Font, FontStyle.Bold);
+                labelMessage.Font = new Font(labelMessage.Font, FontStyle.Regular); // Yes? Make it regular
             }
             else
             {
-                labelMessage.Font = new Font(labelMessage.Font, FontStyle.Regular);
+                labelMessage.Font = new Font(labelMessage.Font, FontStyle.Bold); // No? Make it so.
             }
 
         }
 
-
         private void buttonTitle_Click(object sender, EventArgs e)
         {
+            // this.Text refers to the text in the title of the window. 
+            // If we wanted, we could set that on load by adding the option directly below "InitializeComponent"
             this.Text = "New title";
+        }
+
+        private void CloseApp()
+        {
+            // This is a function.
+            // It doesn't have to return anything, so it's void.
+            // It only applies to this Form specifically, so it's private.
+            // We'd add another arbitrary cs file to define "public" items.
+            // If it needed to return a string, it would be private string name()
+            // If it took argument/variables, they'd be defined in the brackets like so:
+            // private bool function(string arg1, string arg2)
+            Application.Exit();
         }
     }
 }
